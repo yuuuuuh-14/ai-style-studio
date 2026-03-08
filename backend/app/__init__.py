07d27d522060deed647ef8354306b4aa7a38a416
yuuuuuh-14 <youryuha14@icloud.com>
2026-03-08 13:23:16 +0900

    chore: Initialize Python virtual environment and install core dependencies.

---
+++ backend/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    from . import routes
+    app.register_blueprint(routes.bp)
+
+    return app
